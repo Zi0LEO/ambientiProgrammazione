b85ed926a298bc3cbde441524c9ee55c0f6169f3
thecoder <leonardo.npl.02@gmail.com>
2024-11-05 09:45:32 +0100

    fine-tuned syntax

--- shellExercises/mvFileToDir.sh
+++ shellExercises/mvFileToDir.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -f $1 -a -d $2 -a -w $2 ]; then
+if [[ $# ne 2 ]]; then
+    echo "wrong syntax: mvFileToDir file directory";
+    exit 1;
+fi
+
+if [[ -f $1 && -d $2 && -w $2 ]]; then
     mv $1 $2;
+else
+    echo "files do not exist or insufficient permissions";
+    exit 1;
 fi
